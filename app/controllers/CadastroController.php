2519d9bf39835f34692c70d8ad88ee62497c5e66
matheusfterra <matheusfterra@hotmail.com>
2017-08-19 01:34:11 -0300

    Cadastrando usuarios

--- app/controllers/CadastroController.php
+++ app/controllers/CadastroController.php
@@ -1,7 +1,12 @@
 <?php
-
 class CadastroController extends \HXPHP\System\Controller
 {
+	public function cadastrarAction()
+	{
+		$this->view->setFile('index');
 
+		$cadastrarUsuario = User::cadastrar($this->request->post());
+		
+	}
 	
 }
