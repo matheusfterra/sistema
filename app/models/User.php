fd00f941e9587b637c71c3c31d72dde7db3a6a9a
matheusfterra <matheusfterra@hotmail.com>
2017-08-19 23:11:57 -0300

    Alteração de userOBJ para callbackOBJ

--- app/models/User.php
+++ app/models/User.php
@@ -28,17 +28,17 @@ class User extends \HXPHP\System\Model
 	);
 	public static function cadastrar(array $post)
 	{
-		$userObj = new \stdClass;
-		$userObj->user = null;
-		$userObj->status = false;
-		$userObj->errors = array();
+		$callbackObj = new \stdClass;
+		$callbackObj->user = null;
+		$callbackObj->status = false;
+		$callbackObj->errors = array();
 
 		$role = Role::find_by_role('user');
 
-		if(is_null($role))
-			array_push($userObj->errors, 'A role User não existe. Contate o ADM.');
-			return $userObj;
-
+		if(is_null($role)){
+			array_push($callbackObj->errors, 'A role User não existe. Contate o ADM.');
+			return $callbackObj;
+		}else{
 		$post = array_merge($post, array(
 			'role_id' => $role->id,
 			'status' => 1
@@ -50,18 +50,18 @@ class User extends \HXPHP\System\Model
 		$cadastrar = self::create($post);
 
 		if ($cadastrar->is_valid()) {
-			$userObj->user = $cadastrar;
-			$userObj->status = true;
-			return $userObj;
+			$callbackObj->user = $cadastrar;
+			$callbackObj->status = true;
+			return $callbackObj;
 		}
 
 		$errors = $cadastrar->errors->get_raw_errors();
 
 		foreach ($errors as $field => $message) {
-			array_push($userObj->errors, $message[0]);
+			array_push($callbackObj->errors, $message[0]);
 		}
 
-		return $userObj;
+		return $callbackObj;
 
 	}
 }
